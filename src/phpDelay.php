cf61c0701568afbb7036b2541e3c186ad560cedb
YangLong <168119405@qq.com>
2018-10-19 11:32:33 +0800

    update

--- src/phpDelay.php
+++ src/phpDelay.php
@@ -8,38 +8,29 @@ namespace PhpUtils;
  */
 class phpDelay
 {
-    
-    use GetInstances;
-
-    private $tasks = [];
-
-    public function push(callable $closures)
-    {
-        $this->tasks[] = $closures;
-    }
-
-    public function __destruct()
-    {
-        foreach ($this->tasks as $task) {
-            $task();
-        }
-    }
-}
-
-trait GetInstances {
 
     private static $instances = [];
 
     /**
      * @return static
      */
-    final static public function getInstance()
-    {
+    final static public function getInstance() {
         $name = get_called_class();
         if (! isset(self::$instances[$name])) {
             self::$instances[$name] = new static();
         }
         return self::$instances[$name];
     }
-}
 
+    private $tasks = [];
+
+    public function push(callable $closures) {
+        $this->tasks[] = $closures;
+    }
+
+    public function __destruct() {
+        foreach ($this->tasks as $task) {
+            $task();
+        }
+    }
+}
